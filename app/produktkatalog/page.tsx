500eba232a29ea6eb4eccd2f472151e493e11f74
hvnvibz <marketing@dustinwolters.de>
2025-09-01 11:52:52 +0200

    Update product catalog embed to new FlipHTML5 URL

--- app/produktkatalog/page.tsx
+++ app/produktkatalog/page.tsx
@@ -32,7 +32,7 @@ export default function ProduktkatalogPage() {
             <div style={{ position: 'relative', paddingTop: 'max(60%,324px)', width: '100%', height: 0 }}>
               <iframe
                 style={{ position: 'absolute', border: 'none', width: '100%', height: '100%', left: 0, top: 0 }}
-                src="https://online.fliphtml5.com/sattu/epys/"
+                src="https://online.fliphtml5.com/kbjwy/uord/"
                 seamless
                 scrolling="no"
                 frameBorder="0"
